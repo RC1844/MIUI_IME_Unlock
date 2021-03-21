918c25fe4b014efcc90398efb8b9b27521a1750d
RC1844 <1844766234@qq.com>
2021-03-21 18:56:18 +0800

    1.02 优化执行逻辑 部分hook方法不会对定制版进行hook 增加对底视图颜色的修改，颜色完全由输入法控制

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 解锁 MIUI 全面屏优化限制
 
+## 测试环境
+
+LSPosed v1.2.2(API 93.0)
+
+MUI 12.5
+
+Android 11(R,API 30)
+
 ## 使用方法
 
 Xposed API Version >= 93
--- app/src/main/java/com/xposed/miuiime/MainHook.java
+++ app/src/main/java/com/xposed/miuiime/MainHook.java
@@ -3,6 +3,9 @@ package com.xposed.miuiime;
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
 
+import java.util.Arrays;
+import java.util.List;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -10,54 +13,84 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
+import static de.robv.android.xposed.XposedHelpers.callStaticMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 public class MainHook implements IXposedHookLoadPackage {
+    final static List<String> miuiImeList = Arrays.asList("com.iflytek.inputmethod.miui", "com.sohu.inputmethod.sogou.xiaomi", "com.baidu.input_mi", "com.miui.catcherpatch");
+
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         findAndHookMethod("android.inputmethodservice.InputMethodService", lpparam.classLoader, "initViews", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("Hook ServiceInjector: " + lpparam.packageName);
                 InputMethodManager mImm = (InputMethodManager) getObjectField(param.thisObject, "mImm");
+                final boolean contains = !miuiImeList.contains(lpparam.packageName);
                 Class<?> clazz = findClass("android.inputmethodservice.InputMethodServiceInjector", lpparam.classLoader);
-                findAndHookMethod(clazz, "isImeSupport", Context.class, new XC_MethodReplacement() {
-                    @Override
-                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedBridge.log("Hooked ServiceInjector: " + lpparam.packageName);
-                        XposedHelpers.setStaticIntField(clazz, "sIsImeSupport", 1);
-                        return true;
-                    }
-                });
+                if (contains) {
+                    XposedBridge.log("Hook ServiceInjector: " + lpparam.packageName);
+
+                    findAndHookMethod(clazz, "isImeSupport", Context.class, new XC_MethodReplacement() {
+                        @Override
+                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                            // XposedBridge.log("Hooked isImeSupport Method 1: " + lpparam.packageName);
+                            XposedHelpers.setStaticIntField(clazz, "sIsImeSupport", 1);
+                            return true;
+                        }
+                    });
+                    findAndHookMethod("com.android.internal.policy.PhoneWindow", lpparam.classLoader, "setNavigationBarColor", int.class, new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            super.afterHookedMethod(param);
+//                            0xFF141414,0xFFA1A1A1,0x66A1A1A1
+//                            0xFFE7E8EB,0x66000000,0x80000000
+                            callStaticMethod(clazz, "customizeBottomViewColor", true, param.args[0], 0x66747474, 0xff747474);
+                        }
+                    });
+                }
+                // else {
+                //     Class<?> clazz = findClass("android.inputmethodservice.InputMethodServiceInjector",
+                //             lpparam.classLoader);
+                //     findAndHookMethod(clazz, "customizeBottomViewColor", boolean.class, int.class, int.class, int.class,
+                //             new XC_MethodHook() {
+                //                 @Override
+                //                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                //                     super.afterHookedMethod(param);
+                //                     XposedBridge.log("Hooked customizeBottomViewColor Method: " + lpparam.packageName);
+                //                     for (int i = 1; i < param.args.length; i++) {
+                //                         XposedBridge.log("args: " + param.args[i]);
+                //                     }
+                //                 }
+                //             });
+                // }
                 findAndHookMethod("android.inputmethodservice.InputMethodModuleManager", lpparam.classLoader, "loadDex", ClassLoader.class, String.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         super.afterHookedMethod(param);
                         XposedBridge.log("Hook MiuiBottomView: " + lpparam.packageName);
                         final Class<?> clazz = findClass("com.miui.inputmethod.InputMethodBottomManager", (ClassLoader) param.args[0]);
-                        findAndHookMethod(clazz, "isImeSupport", Context.class, new XC_MethodReplacement() {
-                            @Override
-                            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                                XposedBridge.log("Hooked MiuiBottomView 1: " + lpparam.packageName);
-                                XposedHelpers.setStaticIntField(clazz, "sIsImeSupport", 1);
-                                return true;
-                            }
-                        });
-                        findAndHookMethod(clazz, "getSupportIme", new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                XposedBridge.log("Hook MiuiBottomView 2: " + lpparam.packageName);
-                                if (mImm != null) {
-                                    param.setResult(mImm.getEnabledInputMethodList());
-                                    XposedBridge.log("Hooked MiuiBottomView 2: " + lpparam.packageName);
-                                }else {
-                                    XposedBridge.log("Hook MiuiBottomView : List is Null");
-                                    super.beforeHookedMethod(param);
+                        if (contains) {
+                            findAndHookMethod(clazz, "isImeSupport", Context.class, new XC_MethodReplacement() {
+                                @Override
+                                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                                    // XposedBridge.log("Hooked isImeSupport Method 2: " + lpparam.packageName);
+                                    XposedHelpers.setStaticIntField(clazz, "sIsImeSupport", 1);
+                                    return true;
                                 }
-                            }
-                        });
+                            });
+                        }
+                        if (mImm != null) {
+                            XposedBridge.log("Hook getSupportIme Method: " + lpparam.packageName);
+                            findAndHookMethod(clazz, "getSupportIme", new XC_MethodHook() {
+                                @Override
+                                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                    param.setResult(mImm.getEnabledInputMethodList());
+                                    // XposedBridge.log("Hooked getSupportIme Method: " + lpparam.packageName);
+                                }
+                            });
+                        }
                     }
                 });
             }
