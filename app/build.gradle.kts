bfc27785dacafc518c0bd4c656e35b6f18df1f25
keta1 <k@ketal.icu>
2023-07-07 13:11:05 +0800

    1.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.xposed.miuiime"
         minSdk = 28
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.13"
+        versionCode = 12
+        versionName = "1.14"
     }
 
     buildTypes {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.android.application") version "8.0.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
